0f09ec4f4a256c827f8421c3397f5d143e90b19e
kiko fujii <kiko7331@gmail.com>
2019-08-22 02:06:57 +0000

    user login

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,23 @@
+module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+    
+    # if @current_user
+    #   return @current_user
+    # else
+    #   @current_user = User.find_by(id: session[:user_id])
+    #   return @current_user
+    # end
+  end
+
+  def logged_in?
+    !!current_user
+    
+    # if current_user
+    #   return true
+    # else
+    #   return false
+    # end
+  end
+    
+end
