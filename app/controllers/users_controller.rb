e92711c055eb2d205bac91a23ddbc5b96eccec7b
kiko fujii <kiko7331@gmail.com>
2019-08-23 00:13:20 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(id: :desc).page(params[:id])
+    counts(@user)
   end
 
   def new
