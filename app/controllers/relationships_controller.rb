737a4e95d86e45795851f555149fd94888983e5a
kiko fujii <kiko7331@gmail.com>
2019-08-25 12:17:51 +0000

    follow, unfollow

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,16 @@
+class RelationshipsController < ApplicationController
+  before_action :require_user_logged_in
+  def create
+    user = User.find(params[:follow_id])
+    current_user.follow(user)
+    flash[:success] = "フォローしました"
+    redirect_to user
+  end
+
+  def destroy
+    user = User.find(params[:follow_id])
+    current_user.unfollow(user)
+    flash[:success] = "アンフォローしました"
+    redirect_to user
+  end
+end
